7d3e0afca30b0b121b25762a9f0b6f5232dffb1b
Aleksey <bulldog.seregin@gmail.com>
2020-08-02 14:55:13 +0300

    Initial commit

---
+++ server-start.bat
@@ -0,0 +1,2 @@
+java -Xmx6192M -Xms2048M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=4M -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -Dfml.readTimeout=90 -jar fabric-server-launch.jar nogui | win\tee logs\server-full.log
+pause
